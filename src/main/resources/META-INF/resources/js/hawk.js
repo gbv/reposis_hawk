7acc75a6bbee519755bc10ad5d175f70c7e48962
Lennard Golsch <lennard.golsch@icloud.com>
2025-12-08 14:07:06 +0100

    HAWK-35 Improve hawk.js

--- src/main/resources/META-INF/resources/js/hawk.js
+++ src/main/resources/META-INF/resources/js/hawk.js
@@ -1,27 +1,28 @@
-
-$(document).ready(function() {
-
-  // spam protection for mails
-  $('span.madress').each(function(i) {
-      var text = $(this).text();
-      var address = text.replace(" [at] ", "@");
-      $(this).after('<a href="mailto:'+address+'">'+ address +'</a>')
-      $(this).remove();
+// spam protection for mails
+function replaceMaskedEmails() {
+  document.querySelectorAll("span.madress").forEach(span => {
+    const address = span.textContent.replace(" [at] ", "@");
+    const link = document.createElement("a");
+    link.href = `mailto:${address}`;
+    link.textContent = address;
+    span.replaceWith(link);
   });
+}
 
-  // activate empty search on start page
-  $("#hawk-searchMainPage").submit(function (evt) {
-    $(this).find(":input").filter(function () {
-          return !this.value;
-      }).attr("disabled", true);
-    return true;
+function disableEmptyInputsOnSubmit() {
+  const form = document.querySelector("#hawk-searchMainPage");
+  if (!form) return;
+
+  form.addEventListener("submit", () => {
+    form.querySelectorAll("input").forEach(input => {
+      if (!input.value) input.disabled = true;
+    });
   });
+}
 
-  // replace placeholder USERNAME with username
-  var userID = $("#currentUser strong").html();
-  var newHrefTest = 'https://reposis-test.gbv.de/hawk/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
-  $("a[href='https://reposis-test.gbv.de/hawk/servlets/solr/select?q=createdby:USERNAME']").attr('href', newHrefTest);
-  var newHref = 'https://publikationsserver.hawk.de/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
-  $("a[href='https://publikationsserver.hawk.de/servlets/solr/select?q=createdby:USERNAME']").attr('href', newHref);
+function initPage() {
+  replaceMaskedEmails();
+  disableEmptyInputsOnSubmit();
+}
 
-});
+document.addEventListener("DOMContentLoaded", initPage);
